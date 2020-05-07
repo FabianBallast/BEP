36daaca1250076cd27a17a9871b0e7da885e1f5b
gerben2009 <gerben2009@gmail.com>
2020-05-07 15:13:01 +0200

    fixxx

--- Arduino/GridController/GridController.ino
+++ Arduino/GridController/GridController.ino
@@ -1,10 +1,17 @@
 #include "CompentsHfiles/FuelCell.h"
-#include "CompentsHfiles/led_control.h"
+#include "CompentsHfiles/Loads.h"
+
 
 void setup() {
   pinMode(5, OUTPUT);
-  //led_setup();
+  led_setup();
 }
 
 void loop() {
+  analogWrite(5, 100);
+  for (byte i = 0; i <= 100; i++) {\
+    //analogWrite(5, i);
+    load_set(i);
+    delay(50);
+  }
 }
