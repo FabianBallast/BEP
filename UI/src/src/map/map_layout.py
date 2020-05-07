82b19a701bff4ecad8651843aceb2256d8463d1a
Fabian <f.j.p.ballast@student.tudelft.nl>
2020-05-07 21:32:53 +0200

    map_layout nearly done with pythonic conventions

--- UI/src/src/map/map_layout.py
+++ UI/src/src/map/map_layout.py
@@ -9,7 +9,7 @@ class map_layout(QtWidgets.QWidget):
 
     def __init__(self, MainWindow, parent):
         super().__init__(parent)
-        self.MainWindow = MainWindow
+        self.main_window = MainWindow
 
         self.color_on = 'green'
         self.color_off = 'black'
@@ -17,7 +17,7 @@ class map_layout(QtWidgets.QWidget):
 
         width = parent.width()
         height = parent.height()
-
+        
         self.x_pos = [int(width * x) for x in [.340, .515, .685, .090, .890, .090, .528, .718, .925]]
         self.y_pos = [int(height * y) for y in [0.06, 0.06, 0.06, 0.26, 0.32, 0.62, 0.765, 0.78, 0.78]]
         self.diameter = int(width * 0.03)
@@ -26,23 +26,27 @@ class map_layout(QtWidgets.QWidget):
 
         self.create_leds()
         self.create_progress_bar(self.value_progress_bar, width, height)
-        self.create_background(width, height) 
-    
+        self.create_background(width, height)
+
     def create_leds(self):
+        """Create all the LED's and place them in the correct spot."""
 
         for i in range(len(self.x_pos)):
             exec(f"self.LED_{i+1} = Led(parent=self, on_color=Led.{self.color_on}, off_color=Led.{self.color_off}, shape=Led.{self.shape}, build='debug', ind={i})")
             exec(f"self.LED_{i+1}.setGeometry(QtCore.QRect({self.x_pos[i]}, {self.y_pos[i]}, {self.diameter}, {self.diameter}))")
             exec(f"self.LED_{i+1}.setCheckable(False)")
-    
+            
     def create_progress_bar(self, default_value, width, height):
+        """Create the progress bar to visualize the amount of hydrogen stored."""
+
         self.storage_bar = QtWidgets.QProgressBar(self)
         self.storage_bar.setGeometry(QtCore.QRect(int(width * 0.31), int(height * 0.5), int(width * 0.021), int(height * 0.25)))
         self.storage_bar.setProperty("value", default_value)
         self.storage_bar.setOrientation(QtCore.Qt.Vertical)
 
     def create_background(self, width, height):
-
+        """Create a label to display a background image."""
+        
         self.map_picture = QtWidgets.QLabel(self)
         self.map_picture.setGeometry(QtCore.QRect(0, 0, width, height))
         self.map_picture.setPixmap(QtGui.QPixmap(".../img/bg_1.jpg"))
@@ -50,31 +54,33 @@ class map_layout(QtWidgets.QWidget):
         self.map_picture.lower()
 
     def get_current_values(self, solar, wind, demand):
+        """Update the map to match with the current data.
+           Use multiple other functions to accomplish this."""
 
         self.update_progress_bar(solar + wind - demand)
         self.update_background(solar, demand)
         self.update_leds(solar, wind, demand, solar + wind - demand)
-        
-    def button_press(self):
-        
-        led_ind = self.MainWindow.sender().ind + 2 if self.MainWindow.sender().ind < len(self.x_pos) - 1 else 1
-        exec(f"self.LED_{led_ind}.set_status(True)")
     
     def update_progress_bar(self, val):
+        """Üpdate the progress bar to match with the current data."""
+
         val = val / 10
         self.value_progress_bar = max(min(self.value_progress_bar + val, 100), 0) 
         self.storage_bar.setProperty('value', int(self.value_progress_bar))
         self.storage_bar.update()
     
     def update_background(self, solar_power, power_demand):
+        """Update the background to match with the current data."""
+
         solar_power = round(solar_power, 0)
         power_demand = round(power_demand, 0) if self.value_progress_bar > 0 else 0
 
         val = int(6 + min(power_demand, 4) if solar_power > 2 else 1 + min(power_demand, 4)) 
-        self.map_picture.setPixmap(QtGui.QPixmap(f"../img/bg_{str(val)}.jpg"))
+        self.map_picture.setPixmap(QtGui.QPixmap(f".../img/bg_{str(val)}.jpg"))
     
     def update_leds(self, solar, wind, demand, diff):
-        
+        """Update the LED's to match with the current data."""
+
         led_list = []
         solar = round(solar, 0)
         demand = round(demand, 0)
@@ -102,11 +108,11 @@ class map_layout(QtWidgets.QWidget):
         elif demand >= 1 and self.value_progress_bar > 0:
             led_list.append(5)
         
-        for Led in range(1, len(self.x_pos) + 1):
-            if Led in led_list:
-                exec(f"self.LED_{Led}.set_status(True)") 
+        for led in range(1, len(self.x_pos) + 1):
+            if led in led_list:
+                exec(f"self.LED_{led}.set_status(True)")
             else:
-                 exec(f"self.LED_{Led}.set_status(False)") 
+                exec(f"self.LED_{led}.set_status(False)")
         
 
 
