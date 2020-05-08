2e89fb57562f738ca9c32a7bd6d503f451a4779c
gerben2009 <gerben2009@gmail.com>
2020-05-08 15:29:14 +0100

    efiojhfeui

--- PiSerialTest/SerialCommArduino/SerialCommArduino.ino
+++ PiSerialTest/SerialCommArduino/SerialCommArduino.ino
@@ -1,10 +1,25 @@
+#define COMM_SIZE_A 9
+#define COMM_SIZE_P 3
+
+uint8_t comm_received[COMM_SIZE_A];
+uint8_t comm_sent[COMM_SIZE_P];
+
 void setup() {
   Serial.begin(9600);
+  Serial.flush();
 }
 void loop() {
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    Serial.print("You sent me: ");
-    Serial.println(data);
-  }
+//  comm_array[0] = 2;
+//  comm_array[1] = 1;
+//  comm_array[2] = 3;
+  
+ while (Serial.available() < COMM_SIZE_A) {}
+ for (int n=0; n<COMM_SIZE_A; n++)
+    comm_received[n] = Serial.read();
+ 
+  comm_sent[0] = comm_received[0] + comm_received[1] + comm_received[2];
+  comm_sent[1] = comm_received[3] + comm_received[4] + comm_received[5];
+  comm_sent[2] = comm_received[6] + comm_received[7] + comm_received[8];
+  
+  Serial.write(comm_sent, COMM_SIZE_P);
 }
--- PiSerialTest/test.py
+++ PiSerialTest/test.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 import serial
 import time
+import numpy.random as rand
+
+comm_size_to_A = 9
+comm_size_to_P = 3
 
 if __name__ == '__main__':
-   # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser = serial.Serial('COM3', 9600, timeout=0)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+   # ser = serial.Serial('COM3', 9600, timeout=0)
     ser.flush()
 
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+        array_to_send = rand.randint(0,20,comm_size_to_A)
+        bytes_to_send = bytes(list(array_to_send))
+        
+        print('We sent', array_to_send)
+        expected_return = [0,0,0]
+        expected_return[0] = sum(array_to_send[0:3])
+        expected_return[1] = sum(array_to_send[3:6])
+        expected_return[2] = sum(array_to_send[6:9])
+        
+        ser.write(bytes_to_send)
+        print('we expect', expected_return)
+        
+        time.sleep(2)
+        
+        raw_byte_array = ser.read(comm_size_to_P) #.decode('utf-8').rstrip()
+        comm_array = [x for x in raw_byte_array]
+        print(comm_array)
+        
