9b3771e66147fe7baa6427c16c29ed3203cb0932
gerben2009 <gerben2009@gmail.com>
2020-05-08 11:59:40 +0100

    efiojhfeui

--- UI/src/src/backend/Loads.py
+++ UI/src/src/backend/Loads.py
@@ -1,4 +1,4 @@
-
+#pip3 install adafruit-circuitpython-neopixel
 import time
 import board
 import neopixel
@@ -11,9 +11,9 @@ num_pixels = 8
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    led_pixels_pin, num_pixels, brightness=1.0, auto_write=False, pixel_order=ORDER)
+    led_pixels_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
-
+#teste
 
 MODULOCORRECTION = 255 / (100/num_pixels)
 
@@ -26,21 +26,27 @@ MODULOCORRECTION = 255 / (100/num_pixels)
 def load_set (set_value):
     #input a value between 0 and 100
 
-    pixels.clear()
+    #pixels.clear()
     modulo = set_value % num_pixels
     amount_pixels_fully_on = (set_value-modulo)/num_pixels
 
     #set first pixels fully on
-    for pixel_index in range(len(num_pixels)):
+    for pixel_index in range(num_pixels):
         if (pixel_index <=amount_pixels_fully_on):
-            pixels[pixel_index].fill((255, 255, 255))
+            pixels[pixel_index] = (255, 255, 255)
 
         elif (pixel_index == amount_pixels_fully_on+1):     #set 1 pixel partly on
-            intensity_of_half_one = modulo * MODULOCORRECTION
-            pixels[pixel_index].fill(amount_pixels_fully_on+1, pixels.Color(intensity_of_half_one, intensity_of_half_one, intensity_of_half_one))
+            intensity_of_half_one = int(modulo * MODULOCORRECTION)
+            pixels[pixel_index] = (intensity_of_half_one, intensity_of_half_one, intensity_of_half_one)
 
         else: #set rest of pixels fully off
-            pixels[pixel_index].fill((0, 0, 0))
+            pixels[pixel_index] = (0, 0, 0)
 
-
+    print('load' ,set_value)
     pixels.show()
+
+for p in range(100):
+    load_set(p)
+    time.sleep(1)
+
+print(' done' )
