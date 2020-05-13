caa9ebedbc68e444e1628ebcf53e10a3d30c3924
Fabian <f.j.p.ballast@student.tudelft.nl>
2020-05-13 16:29:40 +0200

    Updated non-pi setup

---
+++ UI/src/src/dummy/dummy_io.py
@@ -0,0 +1,20 @@
+"""This module is a fake replacement of IO."""
+
+def setmode(val):
+    pass
+
+def setup(val1, val2):
+    pass
+
+BCM = None
+OUT = None
+
+class PWM:
+    def __init__(self, val1, val2):
+        pass
+
+    def start(self, val1):
+        pass
+
+    def ChangeDutyCycle(self, set_value):
+        pass
