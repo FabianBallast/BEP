423f5e89bbab40c63741525e8913266793972112
Fabian <f.j.p.ballast@student.tudelft.nl>
2020-05-25 14:35:09 +0200

    Fixed serial

--- UI/src/src/system/system.py
+++ UI/src/src/system/system.py
@@ -23,39 +23,68 @@ class System(QtWidgets.QWidget):
         """Create the text and values for the sources."""
         self.source_text = QtWidgets.QLabel(self)
         self.source_text.setFont(self.text_font)
-        self.source_text.setText("Zon: 0W (0%)\nWind: 0W (0%)")
-        self.source_text.setGeometry(QtCore.QRect(int(width * 0.05), int(height * 0.05),
-                                                  int(width * 0.10), int(height * 0.10)))
+        self.source_text.setText("Zon:\nWind:")
+        self.source_text.setGeometry(QtCore.QRect(int(width * 0.05), int(height * 0.10),
+                                                  int(width * 0.10), int(height * 0.15)))
         self.source_text.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
+
+        self.source_figures_curr = QtWidgets.QLabel(self)
+        self.source_figures_curr.setFont(self.text_font)
+        self.source_figures_curr.setText("0W\n0W")
+        self.source_figures_curr.setGeometry(QtCore.QRect(int(width * 0.155), int(height * 0.10),
+                                                          int(width * 0.07), int(height * 0.15)))
+        self.source_figures_curr.setAlignment(QtCore.Qt.AlignTop)
+
+        self.source_figures_per = QtWidgets.QLabel(self)
+        self.source_figures_per.setFont(self.text_font)
+        self.source_figures_per.setText("0W\n0W")
+        self.source_figures_per.setGeometry(QtCore.QRect(int(width * 0.20), int(height * 0.10),
+                                                         int(width * 0.10), int(height * 0.15)))
+        self.source_figures_per.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
     
     def create_hydrogen_text(self, width, height):
         """Create the text for the hydrogen text."""
         self.hydrogen_text = QtWidgets.QLabel(self)
         self.hydrogen_text.setFont(self.text_font)
-        self.hydrogen_text.setText("Verbruik/productie: 0W\nOpslag: 50%")
-        self.hydrogen_text.setGeometry(QtCore.QRect(int(width * 0.45), int(height * 0.70),
-                                                    int(width * 0.10), int(height * 0.10)))
+        self.hydrogen_text.setText("Balans:\nOpslag:")
+        self.hydrogen_text.setGeometry(QtCore.QRect(int(width * 0.415), int(height * 0.70),
+                                                    int(width * 0.10), int(height * 0.15)))
         self.hydrogen_text.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
+
+        self.hydrogen_figures = QtWidgets.QLabel(self)
+        self.hydrogen_figures.setFont(self.text_font)
+        self.hydrogen_figures.setText("0W\n50%")
+        self.hydrogen_figures.setGeometry(QtCore.QRect(int(width * 0.505), int(height * 0.70),
+                                                       int(width * 0.10), int(height * 0.15)))
+        self.hydrogen_figures.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
     
     def create_load_text(self, width, height):
         """Create text for the loads"""
         self.load_text = QtWidgets.QLabel(self)
         self.load_text.setFont(self.text_font)
-        self.load_text.setText("Huizen: 0W (0%)\nIndustrie: 0W (0%)")
-        self.load_text.setGeometry(QtCore.QRect(int(width * 0.85), int(height * 0.05),
-                                                int(width * 0.10), int(height * 0.10)))
+        self.load_text.setText("Huizen:\nIndustrie:")
+        self.load_text.setGeometry(QtCore.QRect(int(width * 0.70), int(height * 0.10),
+                                                int(width * 0.10), int(height * 0.15)))
         self.load_text.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
+
+        self.load_figures_curr = QtWidgets.QLabel(self)
+        self.load_figures_curr.setFont(self.text_font)
+        self.load_figures_curr.setText("0W\n0W")
+        self.load_figures_curr.setGeometry(QtCore.QRect(int(width * 0.75), int(height * 0.10),
+                                                        int(width * 0.08), int(height * 0.15)))
+        self.load_figures_curr.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
+
+        self.load_figures_per = QtWidgets.QLabel(self)
+        self.load_figures_per.setFont(self.text_font)
+        self.load_figures_per.setText("0%\n0%")
+        self.load_figures_per.setGeometry(QtCore.QRect(int(width * 0.85), int(height * 0.10),
+                                                       int(width * 0.10), int(height * 0.15)))
+        self.load_figures_per.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignRight)
     
     def update_text(self, input_data, sensor_data):
         """Update the text."""
-        self.source_text.setText(f"Zon: 0W ({input_data[0]:5.1f}%)\nWind: 0W ({input_data[1]:5.1f}%)")
-        self.hydrogen_text.setText(f"Verbruik/productie: 0W\nOpslag: 50%")
-        self.load_text.setText(f"Huizen: 0W ({input_data[2]:5.1f}%)\nIndustrie: 0W (0%)")
-    
-    def update_system(self, sensor_values):
-        """Update the text related to the sensors."""
-        self.system_text_right.setText(f"\n\n{round(sensor_values[0] / 20, 1)}W\n"
-                                       f"{round(sensor_values[1] / 20, 1)}W\n"
-                                       f"{round(sensor_values[2] / 20, 1)}W\n\n\n\n\n"
-                                       f"{round((sensor_values[0] + sensor_values[1] - sensor_values[2]) / 20, 1)}W\n" #pylint: disable=line-too-long
-                                       f"{round(sensor_values[3], 1)}%")
+        self.source_figures_curr.setText(f"{(sensor_data[0]/20):.2f}W\n{(sensor_data[1]/20):.2f}W")
+        self.source_figures_per.setText(f"{input_data[0]:.1f}%\n{input_data[1]:.1f}%")
+        self.hydrogen_figures.setText(f"{((sensor_data[0] + sensor_data[1] - sensor_data[2])/20):.2f}W\n{sensor_data[3]:.2f}%")
+        self.load_figures_curr.setText(f"{(sensor_data[2]/20):.2f}W\n0.00W")
+        self.load_figures_per.setText(f"{input_data[2]:.1f}%\n0.0%")
